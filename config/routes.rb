d84e5e190e5f45c6ceb396b9ae1761f2f098eb61
WadaAtsushi <tsurupika0901@.gmail.com>
2021-07-06 18:02:47 +0900

    nav bar の link_to

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   post 'likes/:post_id/create' => 'likes#create'
   post 'likes/:post_id/destroy' => 'likes#destroy'
   
-  get 'login' => 'users#login_form'
+  get 'login',to: 'users#login_form', as: 'login'
   post 'userlogin',to:'users#login',as: "user_login"
-  get 'logout' => 'users#logout'
-  get 'users/signin' => 'users#signin'
+  get 'logout', to: 'users#logout', as: 'logout'
+  get 'users/signin', to: 'users#signin', as: 'users_signin'
   post 'users/create', to: 'users#create', as: "signup"
-  get 'users/index' => 'users#index'
+  get 'users/index', to: 'users#index', as: 'user_index'
 
   get '/', to: 'posts#top', as: "top"
-  get 'posts/new' => 'posts#new'
-  post 'posts/create', to: 'posts#create', as: "new_post"
-  patch 'posts/:id/update', to: 'posts#update', as: "edit_post"
-  get 'posts/index', to: 'posts#index', as: "posts_index"   #get "url", to: "controllername#method", as: "name"
-  get 'posts/event_index' => 'posts#event_index'
+  get 'posts/new',to: 'posts#new', as: 'post_new'
+  post 'posts/create', to: 'posts#create', as: 'new_post'
+  patch 'posts/:id/update', to: 'posts#update', as: 'edit_post'
+  get 'posts/index', to: 'posts#index', as: 'posts_index'   #get "url", to: "controllername#method", as: "name"
+  get 'posts/event_index', to: 'posts#event_index', as: 'event_index'
   get 'posts/:id/show' => 'posts#show'
   get 'posts/:id/edit' => 'posts#edit'
   get 'posts/:id/destroy' => 'posts#destroy'
